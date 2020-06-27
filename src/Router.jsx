ea2d030bf5ed2c51be15a4cb3561a20a48e2d13a
Daniel <dslaurindo@gmail.com>
2020-06-27 16:49:14 -0300

    start commit

---
+++ src/Router.jsx
@@ -0,0 +1,26 @@
+// resources
+import React from "react";
+import {Route, Router, Switch} from "react-router-dom";
+// components
+import Header from "./components/header";
+// pages
+import HomePage from "./pages/home/homePage";
+
+class BrowserRouter extends React.Component {
+
+  render() {
+    return (
+      <Router history={this.props.history}>
+        <Switch>
+          <Route component={Header}/>
+        </Switch>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          {/*<Route exact path="/details/:id" component={GamePage}/>*/}
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default BrowserRouter;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../App.css';
+function Header() {
+  return (
+    <header className="header">
+      <img src="https://res.cloudinary.com/luneswallet/image/upload/c_scale,w_411/v1593283625/ei.garotos.games/logo-fortnite.png" alt="fortnite logo"/>
+      <h3>Lista de Items</h3>
+    </header>
+  );
+}
+
+export default Header;
