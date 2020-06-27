c34b66076a0735a40d94853d8b86bad92863ef2e
Daniel <dslaurindo@gmail.com>
2020-06-27 18:35:55 -0300

    Updates

---
+++ precache-manifest.bbee94fd63c9856f7e8abd5bbfcae03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c29087d5bc7ebc7b25cc394d7ec1b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dacd4d34fbe257bb5b1",
+    "url": "/static/css/main.51e962bd.chunk.css"
+  },
+  {
+    "revision": "5cb536afe5bb248cacf8",
+    "url": "/static/js/2.f7935c8d.chunk.js"
+  },
+  {
+    "revision": "925fd4b4a1208799dba805d24acac89e",
+    "url": "/static/js/2.f7935c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dacd4d34fbe257bb5b1",
+    "url": "/static/js/main.5cfeec66.chunk.js"
+  },
+  {
+    "revision": "c663b850a49df4317d9a",
+    "url": "/static/js/runtime-main.ddd8cd19.js"
+  }
+]);
